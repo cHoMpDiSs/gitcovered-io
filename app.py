c2ecadbb14d518e1579922638fd4f019b647ea1d
Jordon Marchesano <jordonomarchesano@gmail.com>
2025-09-09 12:43:28 -0700

    Fix SQLite database path for Heroku environment

--- app.py
+++ app.py
@@ -20,7 +20,10 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__, static_folder='frontend/build/static', static_url_path='/static')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your_secret_key')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.db'
+import tempfile
+
+db_fd, db_path = tempfile.mkstemp()
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # JWT Configuration
